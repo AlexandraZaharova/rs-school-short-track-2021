554d67371be92607802f86a9724e24062e007864
AlexandraZaharova <zakharova911@gmail.com>
2021-04-10 14:25:00 +0300

    feat: implement function encodeLine

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,26 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+  if (str.length === 0) {
+    result += '';
+  } else {
+    const arr = str.split('');
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === arr[0]) {
+        count += 1;
+      } else break;
+    }
+    if (count > 1) {
+      result += count + arr[0];
+    } else result += arr[0];
+    arr.splice(0, count);
+    const str1 = arr.join('');
+    result = str1.length === 0 ? result : result + encodeLine(str1);
+  }
+  return result;
 }
 
 module.exports = encodeLine;
