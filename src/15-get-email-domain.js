d349cdd1a0099aa55dd58db9f11efe1143b3285c
AlexandraZaharova <zakharova911@gmail.com>
2021-04-12 12:40:20 +0300

    feat: implement function getEmailDomain

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,16 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  let result = '';
+  let email1 = email;
+  let x = email1.indexOf('@');
+  while (x !== -1) {
+    result = email1.slice(x + 1);
+    email1 = result;
+    x = email1.indexOf('@');
+  }
+  return result;
 }
 
 module.exports = getEmailDomain;
